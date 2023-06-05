2967f9f173bc1d57588fd83721eaa769301305a5
GabrielDCoutoo <119522432+GabrielDCoutoo@users.noreply.github.com>
2023-06-05 23:29:38 +0100

    update

--- src/script.js
+++ src/script.js
@@ -1,13 +1,12 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls.js';
-import gsap from 'https://cdn.skypack.dev/gsap@3.10.4';
-import Stats from 'https://cdn.skypack.dev/stats.js';
-import dat from 'https://cdn.skypack.dev/dat.gui';
+import { OrbitControls } from 'https://unpkg.com/three@0.119.1/examples/jsm/controls/OrbitControls.js';
+import Stats from 'https://cdnjs.cloudflare.com/ajax/libs/stats.js/7/Stats.min.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import chroma from 'https://cdnjs.cloudflare.com/ajax/libs/chroma-js/2.4.2/chroma.min.js';
 
 console.clear();
 
-let points = {
+const points = {
   everest: new THREE.Vector3(-37.86, 33, 3.2),
   lhotseshar: new THREE.Vector3(-19.86, 29, 36.57),
   nuptse: new THREE.Vector3(-75.49, 23, 26.36),
@@ -15,7 +14,7 @@ let points = {
   lhotse: new THREE.Vector3(-28.86, 29.5, 32.85)
 };
 
-let elTooltips = {
+const elTooltips = {
   everest: document.querySelector('#everest'),
   lhotseshar: document.querySelector('#lhotseshar'),
   nuptse: document.querySelector('#nuptse'),
@@ -23,9 +22,9 @@ let elTooltips = {
   lhotse: document.querySelector('#lhotse')
 };
 
-let elWireframe = document.querySelector('#wireframe');
-let elFill = document.querySelector('#fill');
-let elCompass = document.querySelector('#compass');
+const elWireframe = document.querySelector('#wireframe');
+const elFill = document.querySelector('#fill');
+const elCompass = document.querySelector('#compass');
 let gradient = chroma.scale(['#EFEFEF', '#0c440ec9', '#205375', '#112B3C', '#112B3C']);
 
 if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
@@ -69,7 +68,6 @@ controls.maxPolarAngle = 2;
 controls.minDistance = 50;
 controls.maxDistance = 180;
 
-
 /* Handle auto rotation of the scene */
 let rotationTimeout = null;
 let firstChange = true;
@@ -94,53 +92,28 @@ controls.addEventListener('change', () => {
 });
 
 /* LIGHTS */
-const light = new THREE.HemisphereLight(0xffffff, 0x112B3C, 1);
-scene.add(light);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
 
+const dirLight = new THREE.DirectionalLight(0xffffff, 1);
+dirLight.position.set(-1, 0.75, 1);
+scene.add(dirLight);
+
+/* OBJECTS */
 const loader = new GLTFLoader();
-loader.loadAsync('https://mamboleoo.be/CodePen/random/Brussels/everest.glb', (gltf) => {
-  mountain = gltf.scene.children[0].clone();
-  // Modify and configure the mountain mesh as needed
-
-  // Add the mountain to the scene
-  mountain.name = 'Mountain'; // Set a name to identify the object later
-  group.add(mountain);
-
-  // Create bounding box around the group
-  const boundingBox = new THREE.Box3().setFromObject(group);
-  const boundingGeometry = new THREE.BoxBufferGeometry().setFromPoints(boundingBox.getCorners());
-
-  // Create wireframe helper using the bounding box geometry
-  wireframeHelper = new THREE.LineSegments(
-    new THREE.WireframeGeometry(boundingGeometry),
-    new THREE.LineBasicMaterial({ color: 0xff0000 })
-  );
-  wireframeHelper.visible = false;
-  wireframeHelper.name = 'WireframeHelper'; // Set a name to identify the object later
-  scene.add(wireframeHelper);
-
-  toggleWireframe();
-  toggleFill();
+const material = new THREE.MeshStandardMaterial({
+  color: 0xffffff,
+  roughness: 0.5,
+  metalness: 0.5,
+  side: THREE.DoubleSide
 });
 
-/* SNOWFALL */
-// ... (existing snowfall code)
-
-/* RENDER */
-function render() {
-  requestAnimationFrame(render);
-
-  controls.update();
-
-  // Update the compass
-  let cameraDirection = camera.getWorldDirection(new THREE.Vector3());
-  let theta = Math.atan2(cameraDirection.x, cameraDirection.z);
-  elCompass.style.transform = `rotate(${(theta / (Math.PI * 2)) * 360}deg)`;
-
-  renderer.render(scene, camera);
-}
-
-render();
+loader.load('everest.gltf', (gltf) => {
+  const mesh = gltf.scene.children[0];
+  mesh.material = material;
+  mesh.scale.set(0.5, 0.5, 0.5);
+  group.add(mesh);
+});
 
 /* STATS */
 const stats = new Stats();
@@ -148,47 +121,79 @@ document.body.appendChild(stats.dom);
 
 /* GUI */
 const gui = new dat.GUI();
-gui.add(controls, 'autoRotate').listen();
-gui.add(controls, 'autoRotateSpeed', -10, 10).step(0.1).listen();
-gui.close();
+gui.add(camera.position, 'x', -200, 200);
+gui.add(camera.position, 'y', -200, 200);
+gui.add(camera.position, 'z', -200, 200);
+gui.add(controls, 'autoRotateSpeed', 0, 10);
+gui.add(controls, 'enableRotate');
 
-/* HELPERS */
-// ... (existing helper code)
-
-/* FUNCTIONS */
-function projectTooltips() {
-  const widthHalf = window.innerWidth / 2;
-  const heightHalf = window.innerHeight / 2;
-
-  Object.keys(points).forEach((key) => {
-    const point = points[key];
-    const vector = point.clone().project(camera);
-
-    const x = vector.x * widthHalf + widthHalf;
-    const y = -(vector.y * heightHalf) + heightHalf;
-
-    elTooltips[key].style.transform = `translate3d(${x}px, ${y}px, 0)`;
-  });
-}
-
-function toggleWireframe() {
-  if (wireframeHelper) {
-    wireframeHelper.visible = !wireframeHelper.visible;
-  }
-}
-
-function toggleFill() {
-  if (mountain) {
-    mountain.visible = !mountain.visible;
-  }
-}
-
-window.addEventListener('resize', () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-
-  projectTooltips();
+/* ANIMATION */
+const autoRotation = { timeScale: 1 };
+gsap.to(group.rotation, {
+  y: Math.PI * 2,
+  duration: 60,
+  ease: 'none',
+  repeat: -1,
+  modifiers: {
+    y: gsap.utils.wrap(0, Math.PI * 2)
+  },
+  paused: true,
+  timeScale: autoRotation.timeScale
 });
 
-projectTooltips();
+/* RENDER LOOP */
+function render() {
+  stats.begin();
+
+  const wireframe = elWireframe.checked;
+  const fill = elFill.checked;
+  const compass = elCompass.checked;
+
+  if (wireframe) {
+    group.children.forEach((mesh) => {
+      mesh.material.wireframe = true;
+    });
+  } else {
+    group.children.forEach((mesh) => {
+      mesh.material.wireframe = false;
+    });
+  }
+
+  if (fill) {
+    group.children.forEach((mesh) => {
+      mesh.material.opacity = 1;
+      mesh.material.transparent = false;
+    });
+  } else {
+    group.children.forEach((mesh) => {
+      mesh.material.opacity = 0.5;
+      mesh.material.transparent = true;
+    });
+  }
+
+  if (compass) {
+    controls.target.set(points.everest.x, points.everest.y, points.everest.z);
+  } else {
+    controls.target.set(0, 0, 0);
+  }
+
+  // Update tooltip positions
+  Object.keys(points).forEach((pointName) => {
+    const point = points[pointName];
+    const tooltip = elTooltips[pointName];
+    const pos = point.clone().applyMatrix4(group.matrixWorld);
+    const screenPos = pos.clone().project(camera);
+    const x = (screenPos.x + 1) / 2 * window.innerWidth;
+    const y = (-screenPos.y + 1) / 2 * window.innerHeight;
+    tooltip.style.transform = `translate(${x}px, ${y}px)`;
+  });
+
+  renderer.render(scene, camera);
+
+  stats.end();
+
+  requestAnimationFrame(render);
+}
+
+requestAnimationFrame(render);
+
